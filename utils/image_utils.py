f33b830db86c15bdde76a0f566110f7ebc4e608e
savalena <alena9809@gmail.com>
2022-02-10 18:40:01 +0300

    adding pictures to tensorboard

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,5 +1,6 @@
 from PIL import Image
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def load_image(path):
     img = Image.open(path).convert('RGB')
@@ -12,3 +13,14 @@ def crop_image(img_pil, power=16):
     pix_to_crop_w = w % power
     # img_pil.crop((0, 0, h - pix_to_crop_h, w - pix_to_crop_w))
     return img_pil.crop((0, 0, h - pix_to_crop_h, w - pix_to_crop_w))
+
+
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
